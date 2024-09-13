aeafd18bb6a2159fcc7f4116939b339df38393fa
Sophia <sophia.ffz@gmail.com>
2024-09-12 18:56:03 -0700

    fix

--- tasks/general/summon.py
+++ tasks/general/summon.py
@@ -22,7 +22,7 @@ class Summon(General):
         r = (ticket - 10) // 10
         for i in range(r):
             logger.info(f"Summon round {i + 1}")
-            if not self.wait_until_appear(self.I_SUMMON_AGAIN, 10, 1, click=True, click_delay=3):
+            if not self.wait_until_appear(self.I_SUMMON_AGAIN, 10, 1):
                 break
         self.goto(page_main, page_summon)
         exit()
